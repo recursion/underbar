ac80ff195224ad4a6962f4440e0dd2ef79efb5e8
Michael Symmes <msymmes@gmail.com>
2015-04-03 18:09:10 -0400

    Implemented basic functionality of the 'last' method. First 3 tests now passing.

--- src/underbar.js
+++ src/underbar.js
@@ -38,6 +38,11 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n === 0) {
+      return [];
+    } else {
+      return n === undefined ? array[array.length - 1] : array.slice(n - 1);
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
