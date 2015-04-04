04d276bffb47dcd737715fd999e6ac8564b5bfa7
Michael Symmes <msymmes@gmail.com>
2015-04-04 10:26:02 -0400

    Implemented uniq method so that it is now passing tests. Its amazing how hard I tried to make that. It wasnt nearly as complicated as I wanted it to be...

--- src/underbar.js
+++ src/underbar.js
@@ -32,12 +32,14 @@
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
   _.first = function(array, n) {
+    console.log('First: ' + array.slice(0, n));
     return n === undefined ? array[0] : array.slice(0, n);
   };
 
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    console.log('Last: ' + array.slice(n-1));
     if (n === 0) {
       return [];
     } else if (n > array.length) {
@@ -107,8 +109,17 @@
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+     var result = [];
 
-  };
+    _.each(array, function(value, index, array){
+      // if the value isnt already in our result array, add it.
+      if (_.indexOf(result, value) === -1) {
+        result.push(value);
+      }
+    });
+
+    return result;
+};
 
 
   // Return the results of applying an iterator to each element.
