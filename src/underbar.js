36d859ce5e42f009b860ec6c39e9afa6f2cfd1c1
Michael Symmes <msymmes@gmail.com>
2015-04-03 23:22:26 -0400

    Implemented 'reject' method by building on to the _.filter method.

--- src/underbar.js
+++ src/underbar.js
@@ -96,6 +96,13 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var result = [];
+    _.filter(collection, function(x) {
+      if (!test(x)) {
+        result.push(x);
+      }
+    });
+    return result;
   };
 
   // Produce a duplicate-free version of the array.
